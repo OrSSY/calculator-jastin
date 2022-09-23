47df544710ae19b7a9b0a0e9ce3eced413cc8069
OrSSY <justinhariyono@gmail.com>
2022-09-23 15:36:33 +0700

    done

--- calculator/Program.cs
+++ calculator/Program.cs
@@ -1,62 +1,90 @@
 ﻿using System;
 
-namespace Calc;
+namespace CalculatorJastin;
 
-class Program {  
-    static void Main(string[] args) {  
-        Console.WriteLine("Enter the action to be performed");  
-        Console.WriteLine("Press 1 for Addition");  
-        Console.WriteLine("Press 2 for Subtraction");  
-        Console.WriteLine("Press 3 for Multiplication");  
-        Console.WriteLine("Press 4 for Division \n");  
-        int action = Convert.ToInt32(Console.ReadLine());  
-        Console.WriteLine("Enter 1st input");  
-        int input_1 = Convert.ToInt32(Console.ReadLine());  
-        Console.WriteLine("Enter 2nd input");  
-        int input_2 = Convert.ToInt32(Console.ReadLine());  
-        int result = 0;  
-        switch (action) {  
-            case 1: {  
-                result = Addition(input_1, input_2);  
-                break;  
-            }  
-            case 2: {  
-                result = Subtraction(input_1, input_2);  
-                break;  
-            }  
-            case 3: {  
-                result = Multiplication(input_1, input_2);  
-                break;  
-            }  
-            case 4: {  
-                result = Division(input_1, input_2);  
-                break;  
-            }  
-            default:  
-                Console.WriteLine("Wrong action!! try again");  
-                break;  
-        }  
-        Console.WriteLine("The result is {0}", result);  
-        Console.ReadKey();  
-    }  
+class Program
+{
+    static void Main(string[] args)
+    {
+        int result = 0;
+        Console.WriteLine("Enter the action to be performed");
+        Console.WriteLine("Press 0 to quit the calculator");
+        Console.WriteLine("Press 1 for Addition");
+        Console.WriteLine("Press 2 for Subtraction");
+        Console.WriteLine("Press 3 for Multiplication");
+        Console.WriteLine("Press 4 for Division \n");
+        while (true)
+        {
+            string? command = Console.ReadLine();
+            if (command == null){
+                continue;
+            }
+            if (command.Equals("0")) {
+                Environment.Exit(0);
+            }
+            else if (command.Equals("1") || command.Equals("2") || command.Equals("3") || command.Equals("4")) {
+                Console.WriteLine("enter first number:");
+                int input1 = numberInput();
+                Console.WriteLine("enter second number:");
+                int input2 = numberInput();
+                if (command.Equals("1")){
+                    result = Addition(input1, input2);
+                    break;
+                }
+                if (command.Equals("2")){
+                    result = Subtraction(input1, input2);
+                    break;
+                }
+                if (command.Equals("3")){
+                    result = Multiplication(input1, input2);
+                    break;
+                }
+                if (command.Equals("4")){
+                    result = Division(input1, input2);
+                    break;
+                }
+            }
+            else{
+                Console.WriteLine("wrong command");
+                continue;
+            }
+        }
+    Console.WriteLine("The result is " + result);  
+    }
+    //number inputing   
+    public static int numberInput()
+    {
+        try{
+            int numberInput = Convert.ToInt32(Console.ReadLine());
+            return numberInput;
+        }
+        catch (ArgumentException e) {
+            throw e;
+        }
+    }
     //Addition  
-    public static int Addition(int input_1, int input_2) {  
-        int result = input_1 + input_2;  
-        return result;  
-    }  
+    public static int Addition(int input1, int input2)
+    {
+        int result = input1 + input2;
+        return result;
+    }
     //Substraction  
-    public static int Subtraction(int input_1, int input_2) {  
-        int result = input_1 - input_2;  
-        return result;  
-    }  
+    public static int Subtraction(int input1, int input2)
+    {
+        int result = input1 - input2;
+        return result;
+    }
     //Multiplication  
-    public static int Multiplication(int input_1, int input_2) {  
-        int result = input_1 * input_2;  
-        return result;  
-    }  
+    public static int Multiplication(int input1, int input2)
+    {
+        int result = input1 * input2;
+        return result;
+    }
     //Division  
-    public static int Division(int input_1, int input_2) {  
-        int result = input_1 / input_2;  
-        return result;  
+    public static int Division(int input1, int input2)
+    {
+        int result = input1 / input2;
+        return result;
     }  
-}  
+    }
+
